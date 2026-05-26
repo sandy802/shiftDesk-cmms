d0d330740e221e8a8e71f74ab0b74b55b9f34518
Shubham Ghone <your-github-sghone66@gmail.com>
2026-05-26 09:37:46 +0530

    feat: implement create_ticket

---
+++ ticket_manager.py
@@ -0,0 +1,21 @@
+from storage_stub import StorageStub
+
+class TicketManager:
+
+    def __init__(self):
+        self.storage = StorageStub()
+        
+    def create_ticket(self):
+    pass
+
+    def assign_ticket(self):
+    pass
+    
+    def close_ticket(self):
+    pass
+
+    def list_tickets(self):
+    pass
+
+    def get_ticket(self, ticket_id):
+    pass
